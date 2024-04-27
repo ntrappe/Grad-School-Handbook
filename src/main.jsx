8d25c983d324998a274297f126d925064a620ac7
Nicole Trappe <ntrappe@ucsd.edu>
2024-04-27 13:37:31 -0700

    move css import to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { colors } from '@primer/primitives';
+import './index.css';
 import App from './App.jsx';
+
 
 ReactDOM.createRoot( document.getElementById('root')).render(
   <React.StrictMode>

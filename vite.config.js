4bcd1d26c6a24a210366a74c0f8d37f808a04dbc
Nicole Trappe <ntrappe@ucsd.edu>
2024-04-27 18:39:25 -0700

    serve up index.html for specific routes

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,30 @@
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+const acceptedRoutes = [
+  '/',
+  '/choose-degree',
+];
+
 export default defineConfig({
-  /* set if getting websocket issue when developing */
+  // set if getting websocket issue when developing
   server: {
     port: '5173',
+    // Use the configureServer hook to customize server behavior
     configureServer: ({ app }) => {
+      // Intercept request for specific routes and service index.html
+      // We want a single-page application, not to serve up an html page per url request
       app.use((req, res, next) => {
-        // If the request path is not root, serve index.html
-        if (req.url !== '/') {
-          console.log('url not / is ' + req.url + ' so give index.html');
-          res.sendFile(resolve(__dirname, 'dist', 'index.html'));
+        if (customRoutes.includes(req.url)) {
+          res.sendFile(resolve(__dirname, 'index.html'));
         } else {
-          console.log('url is /');
           next();
         }
       });
     }
   },
-  plugins: [
-    react(),
-    legacy({
-      targets: ['chrome >= 60', 'firefox >= 60', 'safari >= 10', 'edge >= 60', 'ios >= 10'],
-      polyfills: ['es.promise.finally'],
-    }),
-  ],
+  plugins: [react()],
   /* set base to '/' for .github.io or custom website */
   /* set base to '/<repo>/' name otherwise */
   base: '/',
